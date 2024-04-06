5ef1177bbeef767c8e5241eef2b39ab6cc165da6
momo-zaiem <161592132+momo-zaiem@users.noreply.github.com>
2024-04-06 08:32:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,92 +1,22 @@
-import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import ChunkedFileUpload from './ChunkedFileUpload';
+import SingleFileUploader from './SingleFileUploader';
+
 
 function App() {
-   return (
-   <>
+  return (
+    <>
        <h1>React File Upload</h1>
 
        <h2>Single File Upload</h2>
        <SingleFileUploader />
+
+       <h2>Chunked File Upload</h2>
+       <ChunkedFileUpload></ChunkedFileUpload>
     </>
-   );
-   
+
+  );
 }
 
 export default App;
-
-const SingleFileUploader = () => {
-  const [file, setFile] = useState(null);
-  const [status, setStatus] = useState("initial");
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setStatus("initial");
-      setFile(e.target.files[0]);
-    }
-  };
-
-  const handleUpload = async () => {
-    if (file) {
-      setStatus("uploading");
-
-      const formData = new FormData();
-      formData.append("file", file);
-
-      try {
-        const result = await fetch("https://httpbin.org/post", {
-          method: "POST",
-          body: formData,
-        });
-
-        const data = await result.json();
-
-        console.log(data);
-        setStatus("success");
-      } catch (error) {
-        console.error(error);
-        setStatus("fail");
-      }
-    }
-  };
-
-  return (
-    <>
-      <div className="input-group">
-        <label htmlFor="file" className="sr-only">
-          Choose a file
-        </label>
-        <input id="file" type="file" onChange={handleFileChange} />
-      </div>
-      {file && (
-        <section>
-          File details:
-          <ul>
-            <li>Name: {file.name}</li>
-            <li>Type: {file.type}</li>
-            <li>Size: {file.size} bytes</li>
-          </ul>
-        </section>
-      )}
-
-      {file && (
-        <button onClick={handleUpload} className="submit">
-          Upload a file
-        </button>
-      )}
-
-      <Result status={status} />
-    </>
-  );
-};
-
-const Result = ({ status }) => {
-  if (status === "success") {
-    return <p>✅ File uploaded successfully!</p>;
-  } else if (status === "fail") {
-    return <p>❌ File upload failed!</p>;
-  } else if (status === "uploading") {
-    return <p>⏳ Uploading selected file...</p>;
-  } else {
-    return null;
-  }
-};
